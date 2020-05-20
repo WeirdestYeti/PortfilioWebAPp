150e884ae0beb08faa8d5c38e379280a3dad295a
WeirdestYeti <juristheyeti@gmail.com>
2020-05-20 16:39:11 +0200

    New admin layout. TagHelper for active page.

--- scr/Models/SimplePages/Page.cs
+++ scr/Models/SimplePages/Page.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,15 @@ namespace PortfolioWebApp.Models.SimplePages
 {
     public class Page
     {
+        [Key]
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url 
+        {
+            get
+            {
+                return Id + "-" + Name;
+            }
+        }
     }
 }
---
+++ scr/wwwroot/js/dashboard.js
@@ -0,0 +1,8 @@
+﻿/* globals Chart:false, feather:false */
+
+(function () {
+    'use strict'
+
+    feather.replace()
+    
+}())

2ca51fa8ce3dd182bb88e64693b37986899c44ff
WeirdestYeti <juristheyeti@gmail.com>
2020-05-22 21:17:30 +0200

    Update, View list of pages.

---
+++ scr/Areas/Admin/Pages/SimplePages/Edit.cshtml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortfolioWebApp.Models.SimplePages;
+using PortfolioWebApp.Services;
+
+namespace PortfolioWebApp.Areas.Admin.Pages.SimplePages
+{
+    public class EditModel : PageModel
+    {
+        private readonly SimplePageService _simplePageService;
+
+        public EditModel(SimplePageService simplePageService)
+        {
+            _simplePageService = simplePageService;            
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+        [TempData]
+        public int EditId { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [MaxLength(60)]
+            public string Title { get; set; }
+            [MaxLength(60)]
+            [DisplayName("Custorm Url")]
+            public string CustomUrl { get; set; }
+            public string HTML { get; set; }
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if(id != null)
+            {
+                SimplePage simplePage = await _simplePageService.GetPageByIdAsync((int)id);
+                if (simplePage == null) return LocalRedirect("/Admin/SimplePages");
+
+                Input = new InputModel();
+                EditId = simplePage.Id;
+                Input.Title = simplePage.Title;
+                Input.CustomUrl = simplePage.CustomUrl;
+                Input.HTML = simplePage.HTML;
+
+                return Page();
+            }
+            return LocalRedirect("/Admin/SimplePages");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                SimplePage simplePage = new SimplePage();
+
+                if (!string.IsNullOrEmpty(Input.CustomUrl) || !string.IsNullOrWhiteSpace(Input.CustomUrl))
+                {
+                    if (Input.CustomUrl.Contains(" "))
+                    {
+                        ModelState.AddModelError(string.Empty, "Custom Url can't contain spaces.");
+                        return Page();
+                    }
+                    simplePage.CustomUrl = Input.CustomUrl;
+                }
+
+                simplePage.Id = EditId;
+                simplePage.Title = Input.Title;
+                simplePage.HTML = Input.HTML;
+
+                (bool, string) result = await _simplePageService.UpdatePageAsync(simplePage);
+
+                if (!result.Item1)
+                {
+                    ModelState.AddModelError(string.Empty, result.Item2);
+                    return Page();
+                }
+                else
+                {
+                    return LocalRedirect("/Admin/SimplePages");
+                }
+            }
+
+            return Page();
+        }
+    }
+}
--- scr/Services/SimplePageService.cs
+++ scr/Services/SimplePageService.cs
@@ -16,6 +16,11 @@ namespace PortfolioWebApp.Services
             _dbContext = appDbContext;
         }
 
+        /// <summary>
+        /// Adds a new simple page.
+        /// </summary>
+        /// <param name="simplePage"></param>
+        /// <returns>Tupple of bool and string.</returns>
         public async Task<(bool, string)> AddNewPageAsync(SimplePage simplePage)
         {
             if (!string.IsNullOrEmpty(simplePage.CustomUrl))
@@ -32,9 +37,53 @@ namespace PortfolioWebApp.Services
             return (true, "Page successfully added.");
         }
 
+        /// <summary>
+        /// Gets all pages.
+        /// </summary>
+        /// <returns>List of SimplePage</returns>
         public async Task<List<SimplePage>> GetAllPagesAsync()
         {
             return await _dbContext.SimplePages.ToListAsync();
+        }
+        
+        /// <summary>
+        /// Gets page by Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Return page or null if not found.</returns>
+        public async Task<SimplePage> GetPageByIdAsync(int id)
+        {
+            return await _dbContext.SimplePages.SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        /// <summary>
+        /// Updates page.
+        /// </summary>
+        /// <param name="simplePage"></param>
+        /// <returns>Tupple of bool and string.</returns>
+        public async Task<(bool, string)> UpdatePageAsync(SimplePage simplePage)
+        {
+            SimplePage simplePageDb = await _dbContext.SimplePages.SingleOrDefaultAsync(x => x.Id == simplePage.Id);
+            if(simplePage != null)
+            {
+                if (!string.IsNullOrEmpty(simplePage.CustomUrl))
+                {
+                    if (!(await _dbContext.SimplePages.SingleOrDefaultAsync(x => (x.CustomUrl.Equals(simplePage.CustomUrl) && x.Id != simplePage.Id)) == null))
+                    {
+                        return (false, "Custom Url already exists.");
+                    }
+                }
+
+                simplePageDb.Title = simplePage.Title;
+                simplePageDb.CustomUrl = simplePage.CustomUrl;
+                simplePageDb.HTML = simplePage.HTML;
+
+                _dbContext.SimplePages.Update(simplePageDb);
+                await _dbContext.SaveChangesAsync();
+
+                return (true, "Page updated successfully.");
+            }
+            return (false, "Page with the Id: " + simplePage.Id + " not found.");
         }
     }
 }
